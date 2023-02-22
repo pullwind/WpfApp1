51f61c29f898edfec1ce2afe6bf840ca4efed171
whitewolf <whitewolf_asd@outlook.com>
2023-02-23 00:46:19 +0800

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void bt_test(object sender, RoutedEventArgs e)
+		{
+
+			//var str1 = @"Microsoft Windows Desktop Runtime -7.0.0(x86)---- fire";
+			//var str2 = tb_appName.Text;
+
+			//if(str1.Contains(str2,StringComparison.OrdinalIgnoreCase))
+			//{
+			//	MessageBox.Show("yes, found str2 in str1");
+			//}
+			//else
+			//{
+			//	MessageBox.Show("no found");
+			//}
+			//return;
+
+            var listOfRegEntry = new List<string>();
+			var appList = PCinfoClass.getListApp(tb_appName.Text, listOfRegEntry);
+			lb_installedAppInOS.ItemsSource = appList;
+			lb_installedAppInSubkeyOfRegestry.ItemsSource = listOfRegEntry;
+
+
+			if(appList != null )
+			{
+				if(appList.Count > 0)
+				{
+					MessageBox.Show("found installed app list");
+				}
+				else
+				{
+                    MessageBox.Show("no found installed app of target name");
+                }
+
+			}
+
+            if (listOfRegEntry != null)
+            {
+                if (listOfRegEntry.Count > 0)
+                {
+                    MessageBox.Show("found installed app of Reg entry");
+                }
+                else
+                {
+                    MessageBox.Show("no found installed app of target name of Reg entry");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ WpfApp2/PCinfoClass.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public static class StringExtensions
+    {
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source?.IndexOf(toCheck, comp) >= 0;
+        }
+    }
+    internal class PCinfoClass
+	{
+
+        //https://stackoverflow.com/questions/2039186/reading-the-registry-and-wow6432node-key
+
+        public static List<string> getListApp(string target_name, List<string> list2)
+		{
+            var list = new List<string>();
+            StringComparison comp = StringComparison.OrdinalIgnoreCase;
+
+            string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            // if build app in 32bit,  then can not found 7-zip 
+            // if build app in 64bit,  then cat not found filezilla
+            // so here we try seach in different view of registry.
+            Console.WriteLine("--------------32 bit view of Registry");
+            using (var view32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
+            {
+                // 32bit
+                using(var key = view32.OpenSubKey(registry_key,false))
+                {
+                    foreach (string subkey_name in key.GetSubKeyNames())
+                    {
+                        //if (subkey_name.Contains(target_name, comp))
+                        //{
+                        //    list2.Add(subkey_name + "  Found in 32bit registry view");
+                        //}
+
+                        using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                        {
+                            var dn = subkey.GetValue("DisplayName") as string;
+
+
+
+                            if (dn != null)
+                            {
+                                if (dn.Contains(target_name, comp))
+                                {
+                                    list.Add(dn + "  Found in 32bit registry view");
+                                }
+                            }
+
+                            Console.WriteLine(subkey_name + " ---------- " + dn);
+
+                        }
+                    }
+                }
+            }
+
+            //now , open 64bit view of registry
+            Console.WriteLine("--------------64 bit view of Registry");
+            using (var view32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+            {
+                // 32bit
+                using (var key = view32.OpenSubKey(registry_key, false))
+                {
+                    foreach (string subkey_name in key.GetSubKeyNames())
+                    {
+                        //if (subkey_name.Contains(target_name, comp))
+                        //{
+                        //    list2.Add(subkey_name + "  Found in 64bit registry view");
+                        //}
+
+                        using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                        {
+                            var dn = subkey.GetValue("DisplayName") as string;
+
+
+
+                            if (dn != null)
+                            {
+                                if (dn.Contains(target_name, comp))
+                                {
+                                    list.Add(dn + "  Found in 64bit registry view");
+                                }
+                            }
+
+                            Console.WriteLine(subkey_name + " ---------- " + dn);
+
+                        }
+                    }
+                }
+            }
+
+            
+
+            return list; 
+		
+		}	
+
+
+	}
+}
